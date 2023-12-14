583bfaeb0135abfcb249e3d10de3a210c9168f6f
Gana <ganafall9498@gmail.com>
2023-12-14 15:06:50 +0100

    building app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger/swaggerConfig");
+const app = express();
+const port = 3000;
+const userRoute = require("./routes/userRoute");
+
+mongoose.connect("mongodb://127.0.0.1:27017/timer");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", userRoute);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(port, () => {
+  console.log(`Secret Santa app listening on port ${port}`);
+});
